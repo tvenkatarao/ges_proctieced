7f8a87498b39b7ad0bc602bb0dec1ca0c137ded5
venkata rao <t.venkatarao@globaledgesoft.com>
2018-06-27 11:52:40 +0530

    documents and ppt's

---
+++ Documents/documents_pdf/raspi_FT5406DQ9.h
@@ -0,0 +1,16 @@
+/*
+*
+* The macros for the FT5406DQ9 touch screen controller driver.
+*
+*/
+
+
+#define MAX_POINTS 5
+#define SCR_WIDTH 800
+#define SCR_HEIGHT 480
+#define FTS_TOUCH_CONTACT 2
+#define FTS_TOUCH_UP 1
+#define FTS_TOUCH_DOWN 0
+
+
+
